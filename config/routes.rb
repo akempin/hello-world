e2d59e5f79e78989e651e8275ca3b3cda430944c
moo_fighter <moo_fighter@hotmail.com>
2015-03-19 16:16:18 +0000

    Add searchkick gem and implement search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
 
   
 	devise_for :users
+
+
 	  resources :movies do
+	  	collection do
+	  		get 'search'
+	  	end
 	  	resources :reviews, except: [:show, :index]
 	end
 
